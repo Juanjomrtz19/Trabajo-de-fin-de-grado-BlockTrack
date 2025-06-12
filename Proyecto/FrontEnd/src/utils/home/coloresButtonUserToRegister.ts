b26ba1c89111e8dedbbf8f2202971aa6e041b30e
juanjomrtz19 <juanjodev19@gmail.com>
2025-06-13 00:47:55 +0200

    todo actualizado

--- Proyecto/FrontEnd/src/utils/home/coloresButtonUserToRegister.ts
+++
@@ -1,4 +0,0 @@
-export const colorMap: Record<string, string> = {
-    turquesa: "bg-turquesa hover:shadow-2xl shadow-turquesa",
-    rojoSalmon: "bg-rojoSalmon hover:shadow-2xl shadow-rojoSalmon"
-};
